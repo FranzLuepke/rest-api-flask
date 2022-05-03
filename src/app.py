9553a1aa447e684939c27d0a9224268702cf3f11
FranzLuepke <fk.luepke146@uniandes.edu.co>
2022-05-03 10:35:33 -0500

    creation of controllers

--- src/app.py
+++ src/app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, world!"
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
 
 app.run(port=5000)
